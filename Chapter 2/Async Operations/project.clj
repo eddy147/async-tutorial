fdf86517c560718442f179b5b5b4d31eaa5ca565
Eddy <eddy.de.boer@medicore.nl>
2018-11-25 11:19:47 +0100

    Upgraded all with lein ancient

---
+++ Chapter 2/Async Operations/project.clj
@@ -0,0 +1,12 @@
+(defproject understanding-core-async "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.4.490"]
+                 [http-kit "2.3.0"]
+                 [cheshire "5.8.1"]
+                 [org.clojure/data.xml "0.0.8"]
+                 [com.datastax.cassandra/cassandra-driver-core "3.6.0"]
+                 [net.coobird/thumbnailator "0.4.8"]])
